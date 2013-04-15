85782fc82eb68fb60fa4cb9beeec62be0af719c9
Cliff Dailey <valdezdailey@gmail.com>
2013-04-14 20:44:36 -0400

    Add new input fields/CSS changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Omrails::Application.routes.draw do
 
   get 'about' => 'pages#about'
   get 'why' => 'pages#why'
+  get 'pricing' => 'pages#pricing'
   
   get 'users/pins/:id' => 'pins#index_user'
 
--- db/migrate/20130411013814_change_pin_id_to_integer.rb
+++
@@ -1,9 +0,0 @@
-class ChangePinIdToInteger < ActiveRecord::Migration
-   def change
-	  execute %q{
-	    alter table likes
-	    alter column pin_id
-	    type int using cast(pin_id as int)
-	  }
-	end
-end
